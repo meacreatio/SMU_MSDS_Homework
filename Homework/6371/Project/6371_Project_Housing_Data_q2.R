ced3b89e662457f7c3a5829799822c9aef92a84a
Matt Pavlovich <meacreatio@gmail.com>
2017-11-23 12:44:43 -0800

    plots in progress

--- Homework/6371/Project/6371_Project_Housing_Data_q2.R
+++ Homework/6371/Project/6371_Project_Housing_Data_q2.R
@@ -1,3 +1,5 @@
+library("dplyr")
+
 df.train2 <- read.csv("~/Desktop/SMU_MSDS_Homework/Homework/6371/Project/train.csv")
   
 # remove extreme outliers
@@ -16,4 +18,20 @@ df.train2$FireplaceQu <- NULL
 
 fit.full2 <- lm(df.train2$SalePrice ~ ., data = df.train2, na.action = na.exclude)
 summary(fit.full2)
+
+df.train2.steps <-na.omit(df.train2)
+df.train2.numeric <- select_if(df.train2.steps, is.numeric)
+
+fit.steps <- lm(df.train2.steps$SalePrice ~ ., data = df.train2.steps, na.action = na.exclude)
+step(fit.steps, direction="backward")
+fit.backward <- lm(formula = df.train2.steps$SalePrice ~ MSZoning + LotFrontage + 
+                        LotArea + LotShape + LotConfig + Neighborhood + BldgType + 
+                        HouseStyle + OverallQual + OverallCond + YearBuilt + RoofStyle + 
+                        MasVnrType + MasVnrArea + ExterQual + BsmtQual + BsmtExposure + 
+                        BsmtFinSF1 + TotalBsmtSF + CentralAir + X2ndFlrSF + GrLivArea + 
+                        BedroomAbvGr + KitchenQual + GarageYrBlt + GarageCars + OpenPorchSF + 
+                        SaleCondition, data = df.train2.steps, na.action = na.exclude)
+summary(fit.backward)
+vif(fit.backward)
+
 
