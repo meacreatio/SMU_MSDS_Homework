55cd2607d73fbe87caa15ce179d475c80f40f433
Matt Pavlovich <meacreatio@gmail.com>
2017-11-23 13:34:45 -0800

    show plots with ggplot

--- Homework/6371/Project/6371_Project_Housing_Data_q2.R
+++ Homework/6371/Project/6371_Project_Housing_Data_q2.R
@@ -19,9 +19,18 @@ df.train2$FireplaceQu <- NULL
 fit.full2 <- lm(df.train2$SalePrice ~ ., data = df.train2, na.action = na.exclude)
 summary(fit.full2)
 
-df.train2.steps <-na.omit(df.train2)
-df.train2.numeric <- select_if(df.train2.steps, is.numeric)
+# look at the scatter plots to asset for normality
+df.train2.numeric <- select_if(df.train2, is.numeric)
 
+pairs(df.train2.numeric)
+                  
+df.plots <- melt(df.train2.numeric, "SalePrice")
+
+ggplot(df.plots, aes(value, df.plots$SalePrice)) + 
+  geom_point() + 
+  facet_wrap(~variable, scales = "free")
+
+df.train2.steps <-na.omit(df.train2)
 fit.steps <- lm(df.train2.steps$SalePrice ~ ., data = df.train2.steps, na.action = na.exclude)
 step(fit.steps, direction="backward")
 fit.backward <- lm(formula = df.train2.steps$SalePrice ~ MSZoning + LotFrontage + 
