5ef19d40e346dfcf575916c62ad98ccf7313ae30
Matt Pavlovich <meacreatio@gmail.com>
2017-11-22 20:15:00 -0800

    remove centering on response variable

--- Homework/6371/Project/6371_Project_Housing_Data_q1.R
+++ Homework/6371/Project/6371_Project_Housing_Data_q1.R
@@ -19,28 +19,38 @@ fit.full <- lm(df.filtered$logSalePrice ~ df.filtered$LogGrLiveArea + df.filtere
 summary(fit.full)
 
 # aggregate based on neighborhoods that have a positive impact on house prices relative to the others
-df.filtered$Neighborhood <- ifelse(df.filtered$Neighborhood == 'NAmes', 1, 0)
+# df.filtered$Neighborhood <- ifelse(df.filtered$Neighborhood == 'NAmes', 1, 0)
+df.filtered$EncodeN <- ifelse(df.filtered$Neighborhood == "NAmes", 1, 0)
+df.filtered$EncodeE <- ifelse(df.filtered$Neighborhood == "Edwards", 1, 0)
+
+df.filtered$int1 <- df.filtered$EncodeN * df.filtered$GrLivArea
+df.filtered$int2 <- df.filtered$EncodeE * df.filtered$GrLivArea
+
+#temp interaction
+fit.full <- lm(df.filtered$logSalePrice ~ df.filtered$LogGrLiveArea + df.filtered$EncodeN + df.filtered$EncodeE + df.filtered$int1 + df.filtered$int2, data = df.filtered)
+summary(fit.full)
+vif(fit.full)
 
 # scatterplotMatrix(~df.filtered$logSalePrice + df.filtered$LogGrLiveArea + df.filtered$Neighborhood, data=df.filtered)
 
-# add interaction
-df.filtered$Neighborhood <- as.numeric(df.filtered$Neighborhood)
-fit.full <- lm(df.filtered$logSalePrice ~ df.filtered$LogGrLiveArea + df.filtered$Neighborhood + df.filtered$Neighborhood : df.filtered$logSalePrice, data = df.filtered)
-summary(fit.full)
-vif(fit.full)
-
 # add centering since VIF from interaction is very high
-df.filtered$cent1 <- (df.filtered$logSalePrice - mean(df.filtered$logSalePrice)) * (df.filtered$Neighborhood - mean(df.filtered$Neighborhood))
-fit.full <- lm(df.filtered$logSalePrice ~ df.filtered$LogGrLiveArea + df.filtered$Neighborhood + df.filtered$cent1, data = df.filtered)
+df.filtered$cent1 <- (df.filtered$LogGrLiveArea - mean(df.filtered$LogGrLiveArea)) * (df.filtered$EncodeN - mean(df.filtered$EncodeN))
+df.filtered$cent2 <- (df.filtered$LogGrLiveArea - mean(df.filtered$LogGrLiveArea)) * (df.filtered$EncodeE - mean(df.filtered$EncodeE))
+fit.full <- lm(df.filtered$logSalePrice ~ df.filtered$LogGrLiveArea + df.filtered$EncodeN + df.filtered$EncodeE + df.filtered$cent1 + df.filtered$cent2, data = df.filtered)
 summary(fit.full)
 vif(fit.full)
 
+# remove EncodeE due to its high pvalue
+fit.reduced <- lm(df.filtered$logSalePrice ~ df.filtered$LogGrLiveArea + df.filtered$EncodeN + df.filtered$cent1 + df.filtered$cent2, data = df.filtered)
+summary(fit.reduced)
+
+# compare the two models -> including neighborhood contibutes significantly to the model
+anova(fit.full, fit.reduced)
+
+# use the reduced model
 # view the data
 olsrr::ols_rpc_plot(fit.full)
 
-# compare the two models -> including neighborhood contibutes significantly to the model
-fit.reduced <- lm(df.filtered$logSalePrice ~ df.filtered$LogGrLiveArea + df.filtered$Neighborhood, data = df.filtered)
-anova(fit.full, fit.reduced)
 
-plot(fit.full)
+plot(fit.reduced)
 
