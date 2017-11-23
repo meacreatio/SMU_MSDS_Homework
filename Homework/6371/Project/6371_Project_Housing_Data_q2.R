3b429800f149a8b6ea7654f6f6cb1340e0234d31
Matt Pavlovich <meacreatio@gmail.com>
2017-11-23 12:02:43 -0800

    get initial model

--- Homework/6371/Project/6371_Project_Housing_Data_q2.R
+++ Homework/6371/Project/6371_Project_Housing_Data_q2.R
@@ -1,11 +1,19 @@
-df.train <- read.csv("~/Desktop/SMU_MSDS_Homework/Homework/6371/Project/train.csv")
+df.train2 <- read.csv("~/Desktop/SMU_MSDS_Homework/Homework/6371/Project/train.csv")
+  
+# remove extreme outliers
+df.train2 <- df.train2[!(df.train2$Id %in% c(1299,524)), ]
 
 # remove variables with near zero variance
-df.train <- df.train[-nearZeroVar(df.train)]
+library(caret)
+df.train2 <- df.train2[-nearZeroVar(df.train2)]
 
 # find high rate of missing values and remove those variables
-colSums(is.na(df.train))
-df.train$Alley <- NULL
-df.train$PoolQC <- NULL
-df.train$Fence <- NULL
-df.train$FireplaceQu <- NULL
+colSums(is.na(df.train2))
+df.train2$Alley <- NULL
+df.train2$PoolQC <- NULL
+df.train2$Fence <- NULL
+df.train2$FireplaceQu <- NULL
+
+fit.full2 <- lm(df.train2$SalePrice ~ ., data = df.train2, na.action = na.exclude)
+summary(fit.full2)
+
