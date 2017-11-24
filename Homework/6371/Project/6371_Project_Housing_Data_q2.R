d4ba5e6c4a9ebaabb72a5c471309916b1a2ac22c
Matt Pavlovich <meacreatio@gmail.com>
2017-11-23 17:18:56 -0800

    code cleanup

--- Homework/6371/Project/6371_Project_Housing_Data_q2.R
+++ Homework/6371/Project/6371_Project_Housing_Data_q2.R
@@ -93,10 +93,9 @@ df.train2.manual$EncodeNeighborhood <- ifelse(df.train2.manual$Neighborhood == "
 df.train2.manual$EncodeHouseStyle <- ifelse(df.train2.manual$HouseStyle == "1Story", 1, 0)
 df.train2.manual$EncodeBsmtExposure <- ifelse(df.train2.manual$BsmtExposure == "Gd", 1, 0)
 df.train2.manual$EncodeKitchenQual <- ifelse(df.train2.manual$KitchenQual == "TA", 1, 0)
-df.train2.manual$EncodeCondition1 <- ifelse(df.train2.manual$Condition1 == "PosN", 1, 0)
 
 # add interaction
-df.train2.manual$int1 <- df.train2.manual$EncodeNeighborhood * df.train2.manual$OverallQual
+# df.train2.manual$int1 <- df.train2.manual$EncodeNeighborhood * df.train2.manual$OverallQual
 
 fit.manual <- lm(formula = df.train2.manual$SalePrice ~ LotArea + OverallQual + df.train2.manual$EncodeKitchenQual 
                  + GarageCars + df.train2.manual$BsmtQual + df.train2.manual$EncodeBsmtExposure + GrLivArea, 
